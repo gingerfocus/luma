d842c6aa113f041483977d98eb8ff92cd9845fe8
Focus172 <evan.stokdyk@gmail.com>
2023-04-29 21:45:01 -0700

    quility of life

--- README.md
+++ README.md
@@ -7,10 +7,21 @@ Done
 - Deleting
 - Opening
 - Arrowkeys when changing name
-
-Todo
 - Removing output from xdg-open
-- Marking as read
-- Color
+
+
+For 1.0
+- Handling non-utf8 characters
+- Using the file pointed to as input
 - Adding
-- Command (vim)
+    - Pasting
+
+
+Long Term
+- More navigation options
+- Cursor moves better
+- Using multi file input with options to concat or keep seperate
+- Marking as read
+- System Commands inside the thing
+- Color
+- Grab header data from site
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
-use std::{
-    fs,
-    io::{Error, stdout},
-    sync::mpsc::{Sender, self}, cmp::{max, min}
-};
 use crossterm::{
-    event::{read, Event, KeyCode::{*, self}},
+    cursor,
+    event::{read, Event, KeyCode::*},
     execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, ClearType, Clear},
-    cursor
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{
+    cmp::{max, min},
+    fs,
+    io::{stdout, Error},
+    sync::mpsc::{self, Sender},
 };
 
-fn main () -> Result<(), Error> { 
+fn main() -> Result<(), Error> {
     let mut stdout = stdout();
     execute!(stdout, EnterAlternateScreen).unwrap();
     crossterm::terminal::enable_raw_mode().unwrap();
 
     let content = fs::read_to_string("/home/focus/code/lynkd/links.md").unwrap();
 
-    let mut entries = content.lines().filter(|l| !l.is_empty()).map(|line| {
-        let items: Vec<&str> = line.split("](").take(2).collect();
-        let title = items[0].replacen("[", "", 1);
-        let link = items[1].replacen(")", "", 1);
-        Entry { link, title }
-    }).collect::<Vec<Entry>>();
-    
+    let mut entries = content
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|line| {
+            // TODO destructure using a regex
+            let items: Vec<&str> = line.split("](").take(2).collect();
+            let title = items[0].replacen('[', "", 1);
+            let link = items[1].replacen(')', "", 1);
+            Entry { link, title }
+        })
+        .collect::<Vec<Entry>>();
+
     let (tx, rx) = mpsc::channel();
 
-    let rd = Reader { port: tx }; 
+    let rd = Reader { port: tx };
 
     std::thread::spawn(move || {
         rd.read_events().unwrap();
@@ -34,20 +40,22 @@ fn main () -> Result<(), Error> {
 
     let mut index = 0;
     let mut cur_mode = Mode::Normal;
-    let mut rename_offset: u16 = 0;
     let mut run = true;
     while run {
-        // get terminal size
-        //
         execute!(stdout, Clear(ClearType::All)).unwrap();
 
         let (_, height) = term_size::dimensions().unwrap();
         let starting = max(index as i32 - 5, 0) as usize;
 
-        entries.iter().skip(starting).take((height-1) as usize).enumerate().for_each(|(j, entry)| {
-            execute!(stdout, cursor::MoveTo(0, j as u16)).unwrap();
-            print!("{}: {}", j+starting, entry.title);
-        });
+        entries
+            .iter()
+            .skip(starting)
+            .take(height - 1)
+            .enumerate()
+            .for_each(|(j, entry)| {
+                execute!(stdout, cursor::MoveTo(0, j as u16)).unwrap();
+                print!("{}: {}", j + starting, entry.title);
+            });
 
         match &mut cur_mode {
             Mode::Normal => {
@@ -58,7 +66,11 @@ fn main () -> Result<(), Error> {
                 execute!(stdout, cursor::MoveTo(0, height as u16)).unwrap();
                 let msg = format!("R: {}", word.word);
                 print!("{}", msg);
-                execute!(stdout, cursor::MoveTo(word.cursor as u16+ 3, height as u16)).unwrap();
+                execute!(
+                    stdout,
+                    cursor::MoveTo(word.cursor as u16 + 3, height as u16)
+                )
+                .unwrap();
             }
             Mode::Prompt => {
                 let msg = format!("Delete \"{}\"? [y/n]", entries.get(index).unwrap().title);
@@ -70,17 +82,17 @@ fn main () -> Result<(), Error> {
 
         // sleep
         let event = rx.recv().unwrap();
-  
+
         if let Event::Key(kd) = event {
             match (kd.code, &mut cur_mode) {
                 (Esc, _) => {
                     cur_mode = Mode::Normal;
                 }
-                (Char(c), Mode::Rename(word)) => { word.add(c) }
-                (Backspace, Mode::Rename(word)) => { word.del(); }
-                (Char('q'), Mode::Normal) => run = false, 
-                (Char('j'), Mode::Normal) => index += 1,
-                (Char('k'), Mode::Normal) => if index > 0 { index -= 1 },
+                (Char(c), Mode::Rename(word)) => word.add(c),
+                (Backspace, Mode::Rename(word)) => word.del(),
+                (Char('q'), Mode::Normal) => run = false,
+                (Char('j') | Down, Mode::Normal) => index += 1,
+                (Char('k') | Up, Mode::Normal) => index = index.saturating_sub(1),
                 (Enter, Mode::Rename(word)) => {
                     let entry = entries.get_mut(index).unwrap();
                     entry.title = word.word.clone();
@@ -89,27 +101,29 @@ fn main () -> Result<(), Error> {
                 (Enter, Mode::Normal) => {
                     let entry = entries.get(index).unwrap();
                     let link = entry.link.clone();
-                    let _ = std::process::Command::new("xdg-open").arg(link).spawn().unwrap();
+                    _ = std::process::Command::new("xdg-open")
+                        .arg(link)
+                        .output()
+                        .unwrap();
                 }
                 (Char('m'), Mode::Normal) => {
-                    cur_mode = Mode::Rename {
-                        0: EditableWord::new(entries.get(index).unwrap().title.clone())
-                    };
+                    cur_mode =
+                        Mode::Rename(EditableWord::new(entries.get(index).unwrap().title.clone()));
                 }
-                (Char('d'), Mode::Normal) => cur_mode = Mode::Prompt, 
+                (Char('d'), Mode::Normal) => cur_mode = Mode::Prompt,
                 (Char('y'), Mode::Prompt) => {
                     entries.remove(index);
                     cur_mode = Mode::Normal;
                 }
                 (Char('n'), Mode::Prompt) => cur_mode = Mode::Normal,
-                (Left, Mode::Normal) => {
+                (Left | Char('h'), Mode::Normal) => {
                     let entry = entries.remove(index);
-                    entries.insert(index-1, entry);
+                    entries.insert(index - 1, entry);
                     index -= 1;
                 }
-                (Right, Mode::Normal) => {
+                (Right | Char('l'), Mode::Normal) => {
                     let entry = entries.remove(index);
-                    entries.insert(index+1, entry);
+                    entries.insert(index + 1, entry);
                     index += 1;
                 }
                 (Right, Mode::Rename(word)) => word.right(),
@@ -122,9 +136,11 @@ fn main () -> Result<(), Error> {
     crossterm::terminal::disable_raw_mode()?;
     execute!(stdout, LeaveAlternateScreen)?;
 
-    let out = entries.iter().map(|entry| {
-        format!("[{}]({})", entry.title, entry.link)
-    }).collect::<Vec<String>>().join("\n");
+    let out = entries
+        .iter()
+        .map(|entry| format!("[{}]({})", entry.title, entry.link))
+        .collect::<Vec<String>>()
+        .join("\n");
 
     fs::write("/home/focus/code/lynkd/links.md", out)
 }
@@ -144,7 +160,7 @@ impl Reader {
             self.port.send(read()?).unwrap();
         }
     }
-} 
+}
 
 enum Mode {
     Normal,
@@ -165,10 +181,7 @@ struct EditableWord {
 impl EditableWord {
     fn new(word: String) -> Self {
         let cursor = word.len();
-        Self {
-            word,
-            cursor,
-        }
+        Self { word, cursor }
     }
 
     fn add(&mut self, c: char) {
@@ -200,4 +213,3 @@ impl EditableWord {
         }
     }
 }
-
